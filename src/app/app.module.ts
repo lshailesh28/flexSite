542c5c8704cfe971d0298b1b12c4911f55584040
Shailesh <lshailesh28@gmail.com>
2020-04-27 13:42:38 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AboutComponent } from './component/about/about.component';
+import { TestimonialsComponent } from './component/testimonials/testimonials.component';
+import { WorkwithusComponent } from './component/workwithus/workwithus.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    WorkwithusComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/workwithus/workwithus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workwithus',
+  templateUrl: './workwithus.component.html',
+  styleUrls: ['./workwithus.component.scss']
+})
+export class WorkwithusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
